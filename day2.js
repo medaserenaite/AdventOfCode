aebd844f88de32a39d8a5afef986cf213603c4db
meda.serenaite <meda.serenaite@designory.com>
2020-12-02 17:12:59 -0600

    day2 part 2

--- day2.js
+++ day2.js
@@ -1,6 +1,7 @@
 const input = require("fs").readFileSync("day2input.txt", "UTF-8").split("\n")
 
 let masterCounter = 0
+let part2Counter = 0
 
 // iterating through the input
 for(let i=0; i<input.length; i++){
@@ -32,6 +33,18 @@ for(let i=0; i<input.length; i++){
     } else {
         false
     }
+
+    //PART 2
+    let positionA = range[0]
+    let positionB = range[1]
+
+    if(letter == string[positionA-1] || letter == string[positionB-1]){
+        if(!(letter == string[positionA-1] && letter == string[positionB-1])){
+            part2Counter ++
+        } 
+    }
+
 }
-console.log(masterCounter)
-// answer 493
+// PART 1 ANSWER
+// console.log("part1 answer: " + masterCounter) //  493
+console.log(part2Counter) //593
